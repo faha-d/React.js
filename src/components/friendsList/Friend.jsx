e54173ae4db3a4bbd0411cd0723f0bd1f2e53f8b
Fahad <fahad.zahid2004@gmail.com>
2024-11-10 19:17:44 +0000

    useState in object

--- src/components/friendsList/Friend.jsx
+++ src/components/friendsList/Friend.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 const Friends = () => {
   const [friends, setFriends] = useState(["Fahad", "Rafay", "Huzaifa"]);
 
+  const [movie, setMovie] = useState({
+    title: "DeadPool & Wolverine",
+    ratings: 8,
+  });
+
   const addFriend = () => {
     setFriends([...friends, "Talha"]);
   };
 
   const removeFriend = () => {
-    setFriends(
-      friends.filter((f) => {
-        f !== "Rafay";
-      })
-    );
+    setFriends(friends.filter((f) => f !== "Rafay"));
   };
+
+  const updateFriendName = () => {
+    setFriends(friends.map((f) => (f === "Fahad" ? "Muhammad Fahad" : f)));
+  };
+
+  const updateRating = () => {
+    setMovie({...movie, ratings: 5})
+  };
+
   return (
     <>
       {friends.map((friend) => (
@@ -22,6 +32,11 @@ const Friends = () => {
 
       <button onClick={addFriend}>Add Friend</button>
       <button onClick={removeFriend}>Remove Friend</button>
+      <button onClick={updateFriendName}>Update Friend Name</button>
+
+      <h1>{movie.title}</h1>
+      <h2>{movie.ratings}</h2>
+      <button onClick={updateRating}>Update Rating</button>
     </>
   );
 };
