255e96b88fc56724b022e2e21c3d2789babd554b
Fahad <fahad.zahid2004@gmail.com>
2024-11-11 20:22:07 +0000

    Created Todo List App

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,21 +1,42 @@
 import { useState } from "react";
-import "./TodoList.css"
+import { v4 as uuidv4 } from "uuid";
+import { AiOutlineDelete } from "react-icons/ai";
+import "./TodoList.css";
+
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+
+    if (taskInput.trim() === "") return;
+
+    const newTask = { id: uuidv4(), text: taskInput };
+    setTasks([...tasks, newTask]);
+    setTaskInput("");
+  };
+
+  const handleRemoveTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <section className="todo-container">
-
       <header>
         <h1>Todo List</h1>
         <p>Write and Manage your tasks efficiently and smartly</p>
       </header>
 
       <section className="form-container">
-        <form>
+        <form onSubmit={handleAddTask}>
           <div>
             <input
               type="text"
               className="todo-input"
               placeholder="Write your task..."
+              value={taskInput}
+              onChange={(e) => setTaskInput(e.target.value)}
             />
           </div>
 
@@ -25,6 +46,20 @@ const TodoList = () => {
             </button>
           </div>
         </form>
+      </section>
+
+      <section className="list-container">
+        <ul>
+          {tasks.map(({ id, text }) => (
+            <li key={id}>
+              <p>{text}</p>
+              <button onClick={() => handleRemoveTask(id)}>
+                Remove Task
+                <AiOutlineDelete style={{ marginRight: "5px" }} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
